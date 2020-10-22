c09b3db300a7fe125b0e4ed83e4ac0c58aa9b40e
ddb8036631 <ddb8036631@gmail.com>
2020-10-22 16:02:00 +0900

    Resolves S03P31B301-27 / 리액트 기초 유튜브 강의 학습(www.youtube.com/watch?v=LclObYwGj90&list=PLfLgtT94nNq1e6tr4sm2eH6ZZC2jcqGOy&index=1)

--- front-end-museong/src/App.js
+++ front-end-museong/src/App.js
@@ -1,10 +1,59 @@
-import React from 'react';
-import Input from './03/Input';
+/* eslint-disable */
 
-class App extends React.Component {
-  render() {
-    return <Input />;
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  let [title, changeTitle] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+  let [up, changeUp] = useState(0);
+
+  function change() {
+    var newArray = [...title];
+    newArray[0] = '여자 코트 추천';
+    changeTitle(newArray);
   }
+
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <div>개발 Blog</div>
+      </div>
+      <button onClick={change}>버튼</button>
+      <div className="list">
+        <h3>
+          {title[0]}
+          <span
+            onClick={() => {
+              changeUp(up + 1);
+            }}
+          >
+            👍
+          </span>
+          {up}
+        </h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
+
+      <div className="list">
+        <h3>{title[1]}</h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
+
+      <Modal />
+    </div>
+  );
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+      <h2>제목</h2>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  );
 }
 
 export default App;
