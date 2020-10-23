5fa1e5f9131c6376e493e8596bec050a4ec7d582
unknown <ursopretty93@naver.com>
2020-10-23 14:31:13 +0900

    ppt

---
+++ back-end-karam/RoteLearning/src/main/java/com/ssafy/myapp/RoteLearningApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoteLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoteLearningApplication.class, args);
+	}
+
+}
