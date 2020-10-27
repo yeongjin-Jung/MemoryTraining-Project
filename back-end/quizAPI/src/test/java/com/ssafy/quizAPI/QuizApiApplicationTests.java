e92271ebfb934d8b80f420e2c945c459457c21d7
김가람 <ursopretty93@naver.com>
2020-10-27 19:15:34 +0900

    makeBackProject

---
+++ back-end/quizAPI/src/test/java/com/ssafy/quizAPI/QuizApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.quizAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
