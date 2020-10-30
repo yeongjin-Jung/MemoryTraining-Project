80bf3b719c1f2b951a5e68b5d61ec24c3f75db60
ddb8036631 <ddb8036631@gmail.com>
2020-10-30 21:39:04 +0900

    :construction: 세트 만들기 틀 구현중.. 카드 추가시 맨 위로, 애니메이션 적용.

--- front-end/src/pages/SetPage/SetPage.jsx
+++ front-end/src/pages/SetPage/SetPage.jsx
@@ -5,31 +5,46 @@ import './SetPage.css';
 
 import { FiPlus, FiTrash2, FiAlignJustify } from 'react-icons/fi';
 
+import * as _ from 'lodash';
+
 const SetPage = () => {
-  // const [cards, setCards] = useState([{ term: 'T1', meaning: 'M1' }]);
   const [cards, setCards] = useState([]);
+  let orderedCards = [];
 
   var testRef = useRef(null);
+  var contentRef = useRef(null);
   var meaning = useRef(null);
   var divAddCardForm = useRef(null);
 
-  const handleCards = () => {
+  const onDelete = (idx) => {
+    console.log(`onDelete called. idx : ${idx}`);
+    console.log('cards : ', cards);
+    var index = cards.findIndex((i) => i.idx == idx);
+    cards.splice(index, 1);
+
+    let tmpCards = [...cards];
+    for (var i = 0; i < cards.length; i++) {
+      tmpCards[i].idx = cards.length - i - 1;
+    }
+
+    setCards(tmpCards);
+  };
+
+  const addCard = () => {
     console.log('ADD button clicked.');
 
     let inputTerm = divAddCardForm.current.children[1].children[0];
     let textareaMeaning = divAddCardForm.current.children[1].children[1];
-
     let newTerm = inputTerm.value;
     let newMeaning = textareaMeaning.value;
 
     let newObj = {
+      idx: cards.length,
       term: newTerm,
       meaning: newMeaning,
     };
 
-    console.log('before push : ', cards);
     setCards([...cards, newObj]);
-    console.log('after push  : ', cards);
   };
 
   const handleKeyUp = (e) => {
@@ -38,69 +53,37 @@ const SetPage = () => {
     meaning.current.style.height = meaning.current.scrollHeight + 'px';
   };
 
-  const onScroll = (e) => {
-    // console.log(e);
-    // console.log('divAddCardForm : ', divAddCardForm);
-    // console.log('window.scrollY : ', window.scrollY);
-
-    // console.log('divAddCardForm.current.offsetTop : ', divAddCardForm.current.offsetTop);
-    // console.log('window.scrollY : ', window.scrollY);
-
-    // if (testRef.current.offsetTop < window.scrollY) {
-    //   testRef.current.classList.add('fixed');
-    // } else {
-    //   testRef.current.classList.remove('fixed');
-    // }
-    if (divAddCardForm.current.offsetTop < window.scrollY) {
-      divAddCardForm.current.classList.add('fixed');
-    } else {
-      divAddCardForm.current.classList.remove('fixed');
-    }
-
-    console.log(divAddCardForm.current);
-  };
-
-  useEffect(() => {
-    // console.log('testRef : ', testRef);
-    // console.log('testRef.current : ', testRef.current);
-    var setHeaderElement = testRef.current;
-    // console.log('setHeaderElement : ', setHeaderElement);
-    // console.log('setHeaderElement.offsetTop : ', setHeaderElement.offsetTop);
-
-    // console.log('window object : ', window);
-    window.scroll(() => {
-      var window = window.scrollTop;
-      // console.log('스크롤중!');
-    });
-
-    window.addEventListener('scroll', onScroll);
-  });
+  useEffect(() => {});
 
   return (
     <div className="container" style={{ display: 'flex', flexDirection: 'column' }}>
       <div className="set-header" ref={testRef} style={{ width: '80%', height: '250px' }}>
         <div className="" style={{ paddingTop: '1rem' }}>
-          <div className="row">
-            <div className="col-10">
-              <span className="CreateSetHeader-title" style={{ paddingBottom: '2rem' }}>
-                학습 세트 만들기
-              </span>
+          <div style={{ display: 'flex' }}>
+            <div style={{ width: '100%' }}>
+              <div style={{ display: 'flex' }}>
+                <div style={{ width: '500px' }}>
+                  <span className="CreateSetHeader-title">학습 세트 만들기</span>
+                  {/* <span>학습 세트 만들기</span> */}
+                </div>
+                <div style={{ width: '100%', display: 'flex', justifyContent: 'flex-end' }}>
+                  <Button style={{ backgroundColor: 'skyblue', border: 'none' }}>저장</Button>
+                </div>
+              </div>
               <div style={{ marginTop: '2rem' }}>
-                <Form.Control className="inputbox" type="text" placeholder="제목을 입력하세요, 예: 생물학 - 22장: 진화" />
+                <Form.Control className="inputbox" type="text" placeholder="제목을 입력하세요." />
                 <span className="title">제목</span>
                 <br />
-                <Form.Control className="inputbox" type="text" placeholder="설명을 추가하세요." />
+                <Form.Control className="inputbox" type="text" placeholder="설명을 입력하세요." />
                 <span className="description">설명</span>
               </div>
             </div>
-            <div className="col-2">
-              <Button style={{ backgroundColor: 'skyblue', border: 'none' }}>저장</Button>
-            </div>
+            {/* <div style={{ display: 'flex', justifyContent: 'flex-end', width: '10%' }}></div> */}
           </div>
         </div>
       </div>
 
-      <div className="div-add-card-form" style={{ width: '80%', height: '250px' }} ref={divAddCardForm}>
+      <div className="div-add-card-form" style={{ width: '80%', height: '150px' }} ref={divAddCardForm}>
         <span className="CreateSetHeader-title" style={{ paddingBottom: '2rem' }}>
           카드 추가
         </span>
@@ -109,18 +92,62 @@ const SetPage = () => {
           <Form.Control className="inputbox mx-3" type="text" placeholder="단어" style={{ width: '50%' }} />
           <Form.Control className="inputbox mx-3" as="textarea" placeholder="뜻" style={{ width: '50%', height: '38px' }} onKeyUp={handleKeyUp} ref={meaning} />
           <div>
-            <Button type="submit" value="Add" onClick={handleCards}>
-              ADD
+            <Button type="submit" value="Add" onClick={addCard}>
+              <FiPlus />
             </Button>
           </div>
         </div>
         {/* </form> */}
       </div>
 
-      <div className="set-content" style={{ width: '80%', height: '1550px' }}>
-        <span className="CreateSetHeader-title" style={{ paddingBottom: '2rem' }}>
-          카드 목록
-        </span>
+      <div style={{ width: '80%' }}>
+        <div style={{ marginBottom: '2rem' }}>
+          <span className="CreateSetHeader-title">카드 목록</span>
+        </div>
+
+        <div className="set-content" ref={contentRef}>
+          {cards
+            .sort((a, b) => {
+              return b.idx - a.idx;
+            })
+            .map((card) => (
+              <Card card={card} key={card.idx} onDelete={onDelete} />
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Card = ({ card, onDelete }) => {
+  return (
+    <div className="added-card" style={{ marginBottom: '10px' }}>
+      <div stlye={{ display: 'flex', backgroundColor: 'white' }}>
+        <div className="div-card-index" style={{ display: 'flex', borderBottom: '1px solid lightgrey', marginBottom: '1rem' }}>
+          <h2>{card.idx + 1}</h2>
+          <div style={{ width: '100%', display: 'flex', justifyContent: 'flex-end' }}>
+            <FiAlignJustify size="32" />
+            <Button
+              variant="danger"
+              onClick={() => {
+                onDelete(card.idx);
+              }}
+            >
+              <FiTrash2 size="32" />
+            </Button>
+          </div>
+        </div>
+
+        <div style={{ display: 'flex', flexDirection: 'row', marginBottom: '2rem' }}>
+          <div className="mx-3" style={{ width: '40%' }}>
+            <h2 style={{ borderBottom: '5px solid black' }}>{card.term}</h2>
+            <span className="term">단어</span>
+          </div>
+          <div className="mx-3" style={{ width: '40%' }}>
+            <h2 style={{ borderBottom: '5px solid black' }}>{card.meaning}</h2>
+            <span className="descpription">뜻</span>
+          </div>
+        </div>
       </div>
     </div>
   );
