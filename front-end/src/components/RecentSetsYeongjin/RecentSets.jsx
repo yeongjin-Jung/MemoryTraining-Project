5401b1e67a5df48f2a3c495bfca016b997cce7a3
yeongjin <wjddudwls13@naver.com>
2020-10-31 19:05:54 +0900

    최근 세트 항목 애니메이션 오버플로우 방지

--- front-end/src/components/RecentSetsYeongjin/RecentSets.jsx
+++ front-end/src/components/RecentSetsYeongjin/RecentSets.jsx
@@ -8,31 +8,36 @@ const RecentSets = () => {
       <h1 className="title">최근 학습한 세트</h1>
       <div className="card-container">
         <div className="cards-list">
-          <div className="card 1 animate__animated animate__fadeInLeft">
-            <div className="card_content title-white">
-              <p>세트제목</p>
-              <p>세트 작성자</p>
+          <div className="card-overflow">
+            <div className="card 1 animate__animated animate__fadeInLeft">
+              <div className="card_content title-white">
+                <p>세트제목</p>
+                <p>세트 작성자</p>
+              </div>
             </div>
           </div>
-
-          <div className="card 1 animate__animated animate__fadeInUp">
-            <div className="card_content title-white">
-              <p>세트제목</p>
-              <p>세트 작성자</p>
+          <div className="card-overflow">
+            <div className="card 1 animate__animated animate__fadeInUp">
+              <div className="card_content title-white">
+                <p>세트제목</p>
+                <p>세트 작성자</p>
+              </div>
             </div>
           </div>
-
-          <div className="card 1 animate__animated animate__fadeInDown">
-            <div className="card_content title-white">
-              <p> 세트제목</p>
-              <p>세트 작성자</p>
+          <div className="card-overflow">
+            <div className="card 1 animate__animated animate__fadeInDown">
+              <div className="card_content title-white">
+                <p> 세트제목</p>
+                <p>세트 작성자</p>
+              </div>
             </div>
           </div>
-
-          <div className="card 1 animate__animated animate__fadeInRight">
-            <div className="card_content title-white">
-              <p>세트제목</p>
-              <p>세트 작성자</p>
+          <div className="card-overflow">
+            <div className="card 1 animate__animated animate__fadeInRight">
+              <div className="card_content title-white">
+                <p>세트제목</p>
+                <p>세트 작성자</p>
+              </div>
             </div>
           </div>
         </div>
