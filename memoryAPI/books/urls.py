71438192d41f729d27c7c2df421b8dfd0f5e5876
unknown <ursopretty93@naver.com>
2020-11-04 14:23:34 +0900

    [back] Search sets

--- memoryAPI/books/urls.py
+++ memoryAPI/books/urls.py
@@ -5,4 +5,5 @@ app_name = 'books'
 
 urlpatterns = [
     path('create/', views.BookView.as_view(), name='create'),
+    path('search/<str:keyword>/', views.BookList.as_view(), name='search')
 ]
--- memoryAPI/books/views.py
+++ memoryAPI/books/views.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from accounts.serializers import UserSerializer
 from .serializers import BookSerializer, CardSerializer
+from rest_framework import generics
+from .models import Book
 
 class BookView(APIView):
     permission_classes = [AllowAny]
@@ -13,3 +15,10 @@ class BookView(APIView):
         if serializer.is_valid(raise_exception=True):
             serializer.save(user=self.request.user)
         return Response(serializer.data)
+
+class BookList(generics.ListAPIView):
+    serializer_class = BookSerializer
+
+    def get_queryset(self):
+        keyword = self.kwargs['keyword']
+        return Book.objects.filter(title__icontains=keyword).order_by('-updated_at')
