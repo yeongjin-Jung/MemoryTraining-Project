25da221f650b1d1ea8cf2b76c46234da065b87de
hanch <hch9417@naver.com>
2020-11-04 15:02:09 +0900

    docker config

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@ cd /home/memoryAPI
 pip install -r requirements.txt
 cd
 service nginx start
-uwsgi -i /etc/uwsgi/sites/aigaret.ini
-/bin/bash
+uwsgi -i /etc/uwsgi/sites/memorytraining.ini
+#/bin/bash
