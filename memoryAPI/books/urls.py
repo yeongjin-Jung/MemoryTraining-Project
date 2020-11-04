fa7b5a5691a1d9b8c40551918c4c15189929caf3
unknown <ursopretty93@naver.com>
2020-11-04 16:15:29 +0900

    save

--- memoryAPI/books/urls.py
+++ memoryAPI/books/urls.py
@@ -6,4 +6,5 @@ app_name = 'books'
 urlpatterns = [
     path('create/', views.BookView.as_view(), name='create'),
     path('search/<str:keyword>/', views.BookListView.as_view(), name='search'),
+    path('<int:pk>', views.BookDetail.as_View(), name='detail')
 ]
--- memoryAPI/books/views.py
+++ memoryAPI/books/views.py
@@ -1,15 +1,16 @@
 # from django.shortcuts import render
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from accounts.serializers import UserSerializer
 from .serializers import BookSerializer, CardSerializer
 from rest_framework import generics
-from .models import Book
+from .models import Book, Card
 from rest_framework import filters
+from django.http import Http404
 
 class BookView(APIView):
-    permission_classes = [AllowAny]
+    permission_calsses = [IsAuthenticated]
 
     def post(self, request, format=None):
         serializer = BookSerializer(data=request.data)
@@ -19,6 +20,7 @@ class BookView(APIView):
 
 class BookListView(generics.ListAPIView):
     serializer_class = BookSerializer
+    permission_calsses = [IsAuthenticated]
 
     def get_queryset(self):
         user_id = self.request.user.pk
@@ -29,3 +31,11 @@ class BookListView(generics.ListAPIView):
         if self.request.query_params.get('my_set_only'):
             return Book.objects.filter(title__icontains=keyword, user_id=user_id).order_by('-updated_at')
         return Book.objects.filter(title__icontains=keyword).order_by('-updated_at')
+
+class BookDetail(APIView):
+    permission_calsses = [IsAuthenticated]
+
+    def get(self, request, pk, format=None):
+        cards = Card.objects.filter(book_id=pk)
+        serializer = CardSerializer(cards, many=True)
+        return Response(serializer.data)
