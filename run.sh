71e53196a2b5b0045685e3a4667edf790ec79818
hanch <hch9417@naver.com>
2020-11-05 14:01:56 +0900

    fix uwsgi

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ cd /home/s03p31b301/memoryAPI
 pip install -r requirements.txt
 cd
 service nginx start
-uwsgi -i /etc/uwsgi/sites/memorytraining.ini
+uwsgi -i /etc/uwsgi/sites/memorytrain.ini
 /bin/bash
