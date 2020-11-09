8de9ca0db279bad685cc45169aecc29cd01e721f
unknown <ursopretty93@naver.com>
2020-11-09 16:08:07 +0900

    views.py

--- memoryAPI/quizs/views.py
+++ memoryAPI/quizs/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import render
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from books.models import Book, Card, Bookmark
+from books.models import Book, Card
 from rest_framework import status
-# from .serializers import QuizSerializer
 import random
 
 # Create your views here.
@@ -13,13 +12,7 @@ class QuizView(APIView):
     permission_calsses = [IsAuthenticated]
 
     def get(self, request, pk, format=None):
-        book_id = pk
-        if self.request.query_params.get('bookmark'):
-            card_id_list = Bookmark.objects.filter(book=book_id, bookmark_flag=True).values_list('card', flat=True)
-            print(card_id_list)
-            cards = Card.objects.filter(book_id=pk, id__in=card_id_list)
-        else:
-            cards = Card.objects.filter(book_id=pk)
+        cards = Card.objects.filter(book_id=pk)
 
         if cards.count() < 4:
             return Response('객관식 문제 풀기는 한 세트에 4개 이상의 카드가 있어야 가능합니다.', status=status.HTTP_400_BAD_REQUEST)
@@ -38,30 +31,21 @@ class QuizView(APIView):
             Q = val[0]
             A = val[1]
             answers = random.sample(list(quiz.values()), 4)  # 4개 랜덤뽑기
-            print(answers, A)
             # print('index:',k+1, '문제:',Q, '답:',A, '번호:',answers)
             if A not in answers:
                 answers.pop()
                 answers.append(A)
                 random.shuffle(answers)  # 순서 랜덤바꾸기
-            quizbox.append({'question':Q, 'answer':A, 'choice':answers})
+            # quizbox.append({'question':Q, 'answer':A, 'choice':answers})
+            for i in range(len(answers)):
+                if answers[i] == A:
+                    alphabet = chr(97+i)
+
+            quizbox.append({'question':Q, 'a':answers[0], 'b':answers[1], 'c':answers[2], 'd':answers[3], 'answer':alphabet})
+
         random.shuffle(quizbox)
+
         for i, q in enumerate(quizbox):
             q['no'] = i+1
-        # print('=============================')
-        # print(quizbox)
 
-
-        # serializer = QuizSerializer(data=quiz, many=True)
-        # serializer.is_valid()
-        # serializer.validated_data
-        # return Response(serializer.data)
-
-        # quizs = set()
-        # for index, card in enumerate(cards):
-        #     # Post.objects.create(id: index+1, 'Q': card.word, 'A1': card.meaning, 'A2': card.meaning, 'A3': card.meaning, 'A4': card.meaning, 'A': card.meaning)
-        #     quizs.add(quiz_dict)
-        # serializer = QuizSerializer(data=quizs, many=True)
-        # print(serializer.is_valid())
-        # # if serializer.is_valid():
         return Response(quizbox)
