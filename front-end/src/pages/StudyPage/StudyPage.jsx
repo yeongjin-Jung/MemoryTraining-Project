6a57989913834cb43d5363b1d1f2511aa57e08f6
yeongjin <wjddudwls13@naver.com>
2020-11-12 17:59:22 +0900

    tts 기능 소리 꺼짐

--- front-end/src/pages/StudyPage/StudyPage.jsx
+++ front-end/src/pages/StudyPage/StudyPage.jsx
@@ -5,6 +5,7 @@ import CoreStyles from '../StudyPage/styles.scss';
 import AwesomeSliderStyles from 'react-awesome-slider/src/styled/open-animation/open-animation.scss';
 import Flippy, { FrontSide, BackSide } from 'react-flippy';
 import Speech from 'react-speech';
+import { useSpeechSynthesis } from 'react-speech-kit';
 
 import SimpleBar from 'simplebar-react';
 import 'simplebar/dist/simplebar.min.css';
@@ -23,7 +24,6 @@ const ref = React.createRef();
 const StudyPage = (props) => {
   const cards = props.location.state.cardList;
   const book = props.location.state.book;
-
   const [cardList, setCardList] = useState(cards);
 
   useEffect(() => {
@@ -79,13 +79,21 @@ const StudyPage = (props) => {
 };
 
 const Card = ({ data, handleCard, book }) => {
-  console.log('각각의 카드 데이터 : ', data);
-  console.log('각각의 카드 bookmark_flag : ', data.bookmark_flag);
+  const { speak, cancel } = useSpeechSynthesis();
+
+  const [voiceStart, setVoiceStart] = useState(false);
+  const [card, setCard] = useState(data);
+  //console.log('각각의 카드 데이터 : ', data);
+  //console.log('각각의 카드 bookmark_flag : ', data.bookmark_flag);
   // const [color, setColor] = useState(data.bookmark_flag ? 'red' : 'black');
 
-  const [card, setCard] = useState(data);
+  const speechWord = (e) => {
+    speak({ text: data.word });
+    e.stopPropagation();
+  };
 
-  const stopPropagation = (e) => {
+  const speechMeaning = (e) => {
+    speak({ text: data.meaning });
     e.stopPropagation();
   };
 
@@ -93,8 +101,21 @@ const Card = ({ data, handleCard, book }) => {
     console.log('Card useEffect called.');
     console.log('card : ', card);
 
+    var nextClick = document.querySelector('.awssld__next');
+    nextClick.onclick = function () {
+      cancel();
+    };
+
+    var prevClick = document.querySelector('.awssld__prev');
+    prevClick.onclick = function () {
+      cancel();
+    };
     setCard(data);
   });
+
+  const speechhandler = (e) => {
+    console.log(e);
+  };
 
   return (
     <div className="FlippyContainer">
@@ -133,8 +154,8 @@ const Card = ({ data, handleCard, book }) => {
             </button>
           </div>
           <div className="word-type">단어</div>
-          <div className="speech-button" onClick={stopPropagation}>
-            <Speech text={data.word} />
+          <div className="speech-button" onClick={speechWord} style={{ backgroundColor: 'black' }}>
+            {/* <Speech text={data.word} /> */}
           </div>
           <div className="word-content">{data.word}</div>
         </FrontSide>
@@ -167,8 +188,8 @@ const Card = ({ data, handleCard, book }) => {
             </button>
           </div>
           <div className="word-type">뜻</div>
-          <div className="speech-button" onClick={stopPropagation}>
-            <Speech text={data.meaning}></Speech>
+          <div className="speech-button" onClick={speechMeaning} style={{ backgroundColor: 'black' }}>
+            {/* <Speech text={data.meaning}></Speech> */}
           </div>
           <div className="meaning-content">{data.meaning}</div>
         </BackSide>
